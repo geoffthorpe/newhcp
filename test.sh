06f7669086e56f08a1c65539710ceed11bb28619
Geoffrey Thorpe <geoff@geoffthorpe.net>
2024-07-01 13:29:43 -0400

    Start/test KCDs, both primary and secondary
    Signed-off-by: Geoffrey Thorpe <geoff@geoffthorpe.net>

--- test.sh
+++ test.sh
@@ -9,23 +9,41 @@ echo "Running basic sanity test"
 
 do_run() {
 	echo "$1"
-	[[ -n $Q ]] || echo "--> $2"
-	$2 > $OUT 2> $ERR && \
+	FLAGS=""
+	if [[ $2 == "up" ]]; then
+		FLAGS="$FLAGS -d"
+	elif [[ $2 == "run" ]]; then
+		FLAGS="$FLAGS --rm"
+	elif [[ $2 == "down" ]]; then
+		FLAGS="$FLAGS -v"
+	elif [[ $2 == "exec" ]]; then
+		true
+	else
+		echo "Error: unknown docker-compose cmd: $2" >&2
+		exit 1
+	fi
+
+	[[ -n $Q ]] || echo "--> docker-compose $2 $FLAGS $3"
+	docker-compose $2 $FLAGS $3 > $OUT 2> $ERR && \
 		([[ -n $Q ]] || echo "--> SUCCESS") || \
 		exit 1
 }
 
 do_exit() {
-	(Q=1 do_run "Cleaning up" "docker-compose down -v")
+	(Q=1 do_run "Cleaning up" down "")
 }
 
 trap do_exit EXIT
 
 do_run "Starting basic services" \
-	"docker-compose up -d emgmt emgmt_pol erepl arepl ahcp aclient_tpm"
+	up "emgmt emgmt_pol erepl arepl ahcp aclient_tpm kdc_primary_tpm kdc_secondary_tpm kdc_primary_pol kdc_secondary_pol"
 do_run "Fail a premature attestation" \
-	"docker-compose run aclient /hcp/tools/run_client.sh -w"
-do_run "Create and enroll aclient's TPM" \
-	"docker-compose run orchestrator /hcp/tools/run_orchestrator.sh -c -e aclient"
+	run "aclient /hcp/tools/run_client.sh -w"
+do_run "Create and enroll TPMs for aclient, kdc_primary, kdc_secondary" \
+	run "orchestrator /hcp/tools/run_orchestrator.sh -c -e aclient kdc_primary kdc_secondary"
 do_run "Successful attestation" \
-	"docker-compose run aclient /hcp/tools/run_client.sh -R 10 -P 1"
+	run "aclient /hcp/tools/run_client.sh -R 10 -P 1"
+do_run "Starting primary and secondary KDCs" \
+	up "kdc_primary kdc_secondary"
+do_run "Wait for secondary KDC to have realm replicated" \
+	exec "kdc_secondary /hcp/kdcsvc/realm_healthcheck.py -R 10 -P 1"
