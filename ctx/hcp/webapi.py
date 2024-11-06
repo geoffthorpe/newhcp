64abb5c422c53f4c7aecd09ba936a7685e47a17f
Geoffrey Thorpe <geoff@geoffthorpe.net>
2024-11-06 14:42:14 -0500

    webapi: convert white-space, no functional change
    Signed-off-by: Geoffrey Thorpe <geoff@geoffthorpe.net>

--- ctx/hcp/webapi.py
+++ ctx/hcp/webapi.py
@@ -16,42 +16,42 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--healthcheck", action = "store_true",
                help = "check that webapi is running ok")
 parser.add_argument("--hup", action = "store_true",
-		help = "send SIGHUP to currently-running webapi")
+    	help = "send SIGHUP to currently-running webapi")
 parser.add_argument("-R", "--retries", type = int, default = 0,
-		help = "for healthcheck, max # of retries")
+    	help = "for healthcheck, max # of retries")
 parser.add_argument("-P", "--pause", type = int, default = 1,
-		help = "for healthcheck, pause (seconds) between retries")
+    	help = "for healthcheck, pause (seconds) between retries")
 parser.add_argument("-v", "--verbose", default = 0, action = "count",
-		help = "increase output verbosity")
+    	help = "increase output verbosity")
 parser.add_argument("-V", "--less-verbose", default = 0, action = "count",
-		help = "decrease output verbosity")
+    	help = "decrease output verbosity")
 parser.add_argument("-U", "--url", type = str, default = None,
-		help = "URL for 'healthcheck'ing the API")
+    	help = "URL for 'healthcheck'ing the API")
 parser.add_argument("-A", "--curl-args", type = str, default = None,
-		help = "Pre-URL arguments to 'curl'")
+    	help = "Pre-URL arguments to 'curl'")
 parser.add_argument("-C", "--config-section", type = str, default = "webapi",
-		help = "Section name in the JSON config")
+    	help = "Section name in the JSON config")
 args = parser.parse_args()
 myworld = h.hcp_config_extract(".", must_exist = True)
 
 def param(field, _type, required = False, default = None,
-		obj = myworld, objpath = ''):
-	if field in obj:
-		v = obj[field]
-		if not isinstance(v, _type):
-			h.bail(f"'{objpath}.{field}' must be {_type}, not {type(v)}")
-		return v
-	if required:
-		h.bail(f"'{objpath}.{field}' missing but required")
-	return default
+    	obj = myworld, objpath = ''):
+    if field in obj:
+    	v = obj[field]
+    	if not isinstance(v, _type):
+    		h.bail(f"'{objpath}.{field}' must be {_type}, not {type(v)}")
+    	return v
+    if required:
+    	h.bail(f"'{objpath}.{field}' missing but required")
+    return default
 
 myinstance = param('id', str, required = True)
 mydomain = param('default_domain', str, required = True)
 mywebapi = param(args.config_section, dict, required = True)
 
 def webapi_param(field, _type, required = False, default = None):
-	return param(field, _type, required = required, default = default,
-			obj = mywebapi, objpath = f".{mywebapi}")
+    return param(field, _type, required = required, default = default,
+    		obj = mywebapi, objpath = f".{mywebapi}")
 
 myservername = webapi_param('servername', str)
 myport = webapi_param('port', int)
@@ -65,31 +65,31 @@ myuid = webapi_param('uwsgi_uid', str, default = 'www-data')
 mygid = webapi_param('uwsgi_gid', str, default = 'www-data')
 
 if myhttps:
-	def https_param(field, _type, required = True, default = None):
-		return param(field, _type, required, default,
-			obj = myhttps, objpath = f".{mywebapi}.https")
-	myservercert = https_param('certificate', str)
-	myCA = https_param('client_CA', str)
-	myhealthclient = https_param('healthclient', str)
+    def https_param(field, _type, required = True, default = None):
+    	return param(field, _type, required, default,
+    		obj = myhttps, objpath = f".{mywebapi}.https")
+    myservercert = https_param('certificate', str)
+    myCA = https_param('client_CA', str)
+    myhealthclient = https_param('healthclient', str)
 
 if not myservername:
-	myservername = f"{myinstance}.{mydomain}"
+    myservername = f"{myinstance}.{mydomain}"
 if not myport:
-	if myhttps:
-		myport = 443
-	else:
-		myport = 80
+    if myhttps:
+    	myport = 443
+    else:
+    	myport = 80
 myURL = f"{myservername}:{myport}/healthcheck"
 mycurlargs = '-f -g --connect-timeout 2'
 if myhttps:
-	myURL = f"https://{myURL}"
-	mycurlargs = f"{mycurlargs} --cacert {myCA} --cert {myhealthclient}"
+    myURL = f"https://{myURL}"
+    mycurlargs = f"{mycurlargs} --cacert {myCA} --cert {myhealthclient}"
 else:
-	myURL = f"http://{myURL}"
+    myURL = f"http://{myURL}"
 if not args.curl_args:
-	args.curl_args = mycurlargs
+    args.curl_args = mycurlargs
 if not args.url:
-	args.url = myURL
+    args.url = myURL
 
 myetc = f"/etc/hcp/{myservername}/webapi"
 etcnginx = f"{myetc}/nginx"
@@ -100,89 +100,89 @@ lognginx = f"{myvarlog}/nginx"
 myuwsgisock = f"/tmp/{myservername}.uwsgi.sock"
 
 try:
-	verbosity = int(os.environ['VERBOSE'])
+    verbosity = int(os.environ['VERBOSE'])
 except:
-	verbosity = 1
+    verbosity = 1
 
 verbosity = verbosity + args.verbose - args.less_verbose
 h.current_loglevel = verbosity
 os.environ['VERBOSE'] = f"{verbosity}"
 
 if args.healthcheck:
-	h.hlog(1, f"Running: curl {args.curl_args} {args.url}")
-	while True:
-		c = subprocess.run(f"curl {args.curl_args} {args.url}".split(),
-			capture_output = True)
-		if c.returncode == 0:
-			break
-		h.hlog(1, f"Failed with code: {c.returncode}")
-		h.hlog(2, f"Error output:\n{c.stderr}")
-		if args.retries == 0:
-			h.hlog(0, "Failure, giving up")
-			break
-		args.retries = args.retries - 1
-		if args.pause > 0:
-			h.hlog(2, f"Pausing for {args.pause} seconds")
-			time.sleep(args.pause)
-	sys.exit(c.returncode)
+    h.hlog(1, f"Running: curl {args.curl_args} {args.url}")
+    while True:
+    	c = subprocess.run(f"curl {args.curl_args} {args.url}".split(),
+    		capture_output = True)
+    	if c.returncode == 0:
+    		break
+    	h.hlog(1, f"Failed with code: {c.returncode}")
+    	h.hlog(2, f"Error output:\n{c.stderr}")
+    	if args.retries == 0:
+    		h.hlog(0, "Failure, giving up")
+    		break
+    	args.retries = args.retries - 1
+    	if args.pause > 0:
+    		h.hlog(2, f"Pausing for {args.pause} seconds")
+    		time.sleep(args.pause)
+    sys.exit(c.returncode)
 
 if os.path.exists(myetc):
-	myetc_old = f"{myetc}.old"
-	if os.path.exists(myetc_old):
-		h.hlog(1, "Deleting really old webapi config")
-		shutil.rmtree(myetc_old)
-	h.hlog(1, "Moving old webapi config")
-	os.rename(myetc, myetc_old)
+    myetc_old = f"{myetc}.old"
+    if os.path.exists(myetc_old):
+    	h.hlog(1, "Deleting really old webapi config")
+    	shutil.rmtree(myetc_old)
+    h.hlog(1, "Moving old webapi config")
+    os.rename(myetc, myetc_old)
 os.makedirs(myetc)
 
 # Stand up an nginx HTTPS/TLS front-end iff there's a "myhttps" config
 if myhttps:
-	h.hlog(1, f"Putting template nginx config: {etcnginx}")
-	shutil.copytree('/etc/nginx', etcnginx)
-	# TODO: ugh. This feels like the wrong thing to do
-	for r in [ f"s,/etc/nginx,{etcnginx},g",
-			f"s,/var/log/nginx,{lognginx},g",
-			f"s,/run/nginx,/run/{myservername},g" ]:
-		subprocess.run([
-			'bash', '-c',
-			f'find {etcnginx}/ -type f | xargs perl -pi -e {r}'])
-	if os.path.exists(f"{etcnginx}/sites-enabled/default"):
-		h.hlog(1, "Removing site file [...]/sites-enabled/default")
-		os.remove(f"{etcnginx}/sites-enabled/default")
-	h.hlog(1, f"Injecting site file [...]/sites-enabled/{myservername}")
-	open(f"{etcnginx}/sites-enabled/{myservername}", 'w').write('''
+    h.hlog(1, f"Putting template nginx config: {etcnginx}")
+    shutil.copytree('/etc/nginx', etcnginx)
+    # TODO: ugh. This feels like the wrong thing to do
+    for r in [ f"s,/etc/nginx,{etcnginx},g",
+    		f"s,/var/log/nginx,{lognginx},g",
+    		f"s,/run/nginx,/run/{myservername},g" ]:
+    	subprocess.run([
+    		'bash', '-c',
+    		f'find {etcnginx}/ -type f | xargs perl -pi -e {r}'])
+    if os.path.exists(f"{etcnginx}/sites-enabled/default"):
+    	h.hlog(1, "Removing site file [...]/sites-enabled/default")
+    	os.remove(f"{etcnginx}/sites-enabled/default")
+    h.hlog(1, f"Injecting site file [...]/sites-enabled/{myservername}")
+    open(f"{etcnginx}/sites-enabled/{myservername}", 'w').write('''
 server {{
-	listen                 {myport} ssl;
-	server_name	       {myservername};
-	ssl_certificate        {myservercert};
-	ssl_certificate_key    {myservercert};
-	ssl_client_certificate {myCA};
-	ssl_verify_client      on;
+    listen                 {myport} ssl;
+    server_name	       {myservername};
+    ssl_certificate        {myservercert};
+    ssl_certificate_key    {myservercert};
+    ssl_client_certificate {myCA};
+    ssl_verify_client      on;
 
-	location / {{
-		# Pass the standard stuff along that the distro's default nginx
-		# install likes to pass along.
-		include        uwsgi_params;
-		uwsgi_read_timeout {myclienttimeout}s;
-		uwsgi_send_timeout {myclienttimeout}s;
+    location / {{
+    	# Pass the standard stuff along that the distro's default nginx
+    	# install likes to pass along.
+    	include        uwsgi_params;
+    	uwsgi_read_timeout {myclienttimeout}s;
+    	uwsgi_send_timeout {myclienttimeout}s;
 
-		# This is where uwsgi will be expecting us
-		uwsgi_pass     unix:{myuwsgisock};
-		# Pass the extra stuff that _we_ want the flask app to get
-		uwsgi_param    SSL_CLIENT_CERT           $ssl_client_cert;
-		uwsgi_param    SSL_CLIENT_S_DN           $ssl_client_s_dn;
-		uwsgi_param    SSL_CLIENT_S_DN_LEGACY    $ssl_client_s_dn_legacy;
-	}}
+    	# This is where uwsgi will be expecting us
+    	uwsgi_pass     unix:{myuwsgisock};
+    	# Pass the extra stuff that _we_ want the flask app to get
+    	uwsgi_param    SSL_CLIENT_CERT           $ssl_client_cert;
+    	uwsgi_param    SSL_CLIENT_S_DN           $ssl_client_s_dn;
+    	uwsgi_param    SSL_CLIENT_S_DN_LEGACY    $ssl_client_s_dn_legacy;
+    }}
 }}
 '''.format(myport = myport, myservername = myservername,
-		myservercert = myservercert, myCA = myCA,
-		myclienttimeout = myclienttimeout, myuwsgisock = myuwsgisock))
-	# Create log directory
-	# TODO: add ownership/chmod controls?
-	h.hlog(1, "Creating nginx log dir")
-	os.makedirs(lognginx, exist_ok = True)
-	h.hlog(1, "Starting nginx")
-	subprocess.run([ 'nginx', '-c', f"{etcnginx}/nginx.conf" ])
+    	myservercert = myservercert, myCA = myCA,
+    	myclienttimeout = myclienttimeout, myuwsgisock = myuwsgisock))
+    # Create log directory
+    # TODO: add ownership/chmod controls?
+    h.hlog(1, "Creating nginx log dir")
+    os.makedirs(lognginx, exist_ok = True)
+    h.hlog(1, "Starting nginx")
+    subprocess.run([ 'nginx', '-c', f"{etcnginx}/nginx.conf" ])
 
 # Duplicate the HCP_CONFIG_FILE so its readable by the app, and if a
 # ".webapi.config" is specified, duplicate that too.
@@ -192,22 +192,22 @@ hcpcfg_old = os.environ['HCP_CONFIG_FILE']
 hcpcfg_new = f"{etcjson}/hcp_config_file"
 
 if myappcfg:
-	newappcfg = f"{etcjson}/app_config_file"
-	h.hlog(1, f"Migrating app config: {myappcfg} -> {newappcfg}")
-	shutil.copyfile(myappcfg, newappcfg)
-	os.chmod(newappcfg, 0o444)
-	# Replace {myappcfg} with {newappcfg}
-	myworld['webapi']['config'] = newappcfg
+    newappcfg = f"{etcjson}/app_config_file"
+    h.hlog(1, f"Migrating app config: {myappcfg} -> {newappcfg}")
+    shutil.copyfile(myappcfg, newappcfg)
+    os.chmod(newappcfg, 0o444)
+    # Replace {myappcfg} with {newappcfg}
+    myworld['webapi']['config'] = newappcfg
 h.hlog(1, f"Migrating HCP config: {os.environ['HCP_CONFIG_FILE']} -> {hcpcfg_new}")
 with open(hcpcfg_new, 'w') as fp:
-	json.dump(myworld, fp)
+    json.dump(myworld, fp)
 os.chmod(hcpcfg_new, 0o444)
 os.environ['HCP_CONFIG_FILE'] = hcpcfg_new
 
 # Produce the uwsgi config
 h.hlog(1, f"Producing uwsgi config: {etcuwsgi}")
 with open(etcuwsgi, 'w') as fp:
-	fp.write('''[uwsgi]
+    fp.write('''[uwsgi]
 master = true
 processes = 2
 threads = 2
@@ -219,26 +219,26 @@ die-on-term = true
 route-if = equal:${{PATH_INFO}};/healthcheck donotlog:
 harakiri = {myharakiri}
 '''.format(myuid = myuid, mygid = mygid, myapp = myapp, myharakiri = myharakiri))
-	for k in myenv:
-		fp.write(f"env = {k}={myenv[k]}\n")
-	if myhttps:
-		fp.write('''socket = {myuwsgisock}
+    for k in myenv:
+    	fp.write(f"env = {k}={myenv[k]}\n")
+    if myhttps:
+    	fp.write('''socket = {myuwsgisock}
 socket-timeout = {myclienttimeout}
 chmod-socket = 660
 vacuum = true
 '''.format(myuwsgisock = myuwsgisock, myclienttimeout = myclienttimeout))
-	else:
-		fp.write('''plugin = http
+    else:
+    	fp.write('''plugin = http
 http = :{myport}
 http-timeout = {myclienttimeout}
 stats = :{mystats}
 '''.format(myport = myport, myclienttimeout = myclienttimeout,
-		mystats = myport + 1))
+    	mystats = myport + 1))
 
 bin_uwsgi_python = shutil.which('uwsgi_python3')
 if not bin_uwsgi_python:
-	# On debian buster+bullseye+bookworm we have a '37' (but no '3')
-	# On debian trixie we have a "312"
-	bin_uwsgi_python = shutil.which('uwsgi_python312')
+    # On debian buster+bullseye+bookworm we have a '37' (but no '3')
+    # On debian trixie we have a "312"
+    bin_uwsgi_python = shutil.which('uwsgi_python312')
 h.hlog(1, f"Starting uwsgi")
 subprocess.run([ bin_uwsgi_python, '--ini', etcuwsgi ])
