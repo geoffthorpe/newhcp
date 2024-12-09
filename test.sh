e055c8f6146cbe15ec424d393ae4252e699bfcfb
Geoffrey Thorpe <geoff@geoffthorpe.net>
2024-12-08 21:38:05 -0500

    usecase: move 'ssherver' to 'ssherver1'
    Signed-off-by: Geoffrey Thorpe <geoff@geoffthorpe.net>

--- test.sh
+++ test.sh
@@ -40,8 +40,8 @@ do_exit() {
 STARTSERVICES="policy emgmt erepl arepl ahcp aclient_tpm"
 STARTSERVICES="$STARTSERVICES kdc_primary kdc_secondary kdc_keytab"
 STARTSERVICES="$STARTSERVICES kdc_primary_tpm kdc_secondary_tpm kdc_keytab_tpm"
-STARTSERVICES="$STARTSERVICES ssherver workstation1 bigbrother www"
-STARTSERVICES="$STARTSERVICES ssherver_tpm workstation1_tpm www_tpm"
+STARTSERVICES="$STARTSERVICES ssherver1 workstation1 bigbrother www"
+STARTSERVICES="$STARTSERVICES ssherver1_tpm workstation1_tpm www_tpm"
 do_run "Starting basic services" \
 	up "$STARTSERVICES"
 do_run "Fail a premature attestation" \
@@ -54,19 +54,19 @@ do_run "Wait for secondary KDC to have realm replicated" \
 	exec "kdc_secondary /hcp/kdcsvc/realm_healthcheck.py -R 10 -P 1"
 do_run "Create and enroll remaining TPMs" \
 	run "orchestrator /hcp/tools/run_orchestrator.sh -c -e"
-do_run "Wait for ssherver to be ready" \
-	exec "ssherver /hcp/sshd.py --healthcheck -R 10 -P 1"
+do_run "Wait for ssherver1 to be ready" \
+	exec "ssherver1 /hcp/sshd.py --healthcheck -R 10 -P 1"
 do_run "Wait for workstation1 to be ready" \
 	exec "workstation1 /hcp/attested.py -R 10 -P 1"
 sso_cmd="/install-heimdal/bin/kinit -C FILE:/home/luser/.hcp/pkinit/user-luser-key.pem luser"
-sso_cmd="$sso_cmd ssh -l luser ssherver.hcphacking.xyz echo"
+sso_cmd="$sso_cmd ssh -l luser ssherver1.hcphacking.xyz echo"
 sso_cmd="$sso_cmd \"This output indicates successful SSO+ssh\""
-do_run "Do SSO login from workstation1 to ssherver as 'luser'" \
+do_run "Do SSO login from workstation1 to ssherver1 as 'luser'" \
 	exec "workstation1 $sso_cmd"
 do_run "Wait for bigbrother to be ready" \
 	exec "bigbrother /hcp/attested.py -R 10 -P 1"
 sso_cmd="/install-heimdal/bin/kinit -C FILE:/root/.hcp/pkinit/user-root-key.pem root"
-sso_cmd="$sso_cmd ssh -l root ssherver.hcphacking.xyz echo"
+sso_cmd="$sso_cmd ssh -l root ssherver1.hcphacking.xyz echo"
 sso_cmd="$sso_cmd \"If root==\$(whoami) then success\""
-do_run "Do SSO login from bigbrother to ssherver as 'root'" \
+do_run "Do SSO login from bigbrother to ssherver1 as 'root'" \
 	exec "bigbrother $sso_cmd"
