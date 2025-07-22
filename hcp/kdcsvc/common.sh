8f184d56b9f70cda783ff9cb152ee9fca2093327
Geoffrey Thorpe <geoff@geoffthorpe.net>
2025-07-22 11:34:55 -0400

    kdcsvc: namespace parameters are now overridable
    Signed-off-by: Geoffrey Thorpe <geoff@geoffthorpe.net>

--- hcp/kdcsvc/common.sh
+++ hcp/kdcsvc/common.sh
@@ -14,18 +14,26 @@ export HCP_KDCSVC_NAMESPACE=$(echo "$HCP_KDCSVC_JSON" | jq -r ".namespace")
 export HCP_KDCSVC_POLICYURL=$(echo "$HCP_KDCSVC_JSON" | jq -r ".policy_url")
 export HCP_KDCSVC_ANCHOR=$(echo "$HCP_KDCSVC_JSON" | jq -r ".anchor")
 export HCP_KDCSVC_PKINIT_IDENTITY=$(echo "$HCP_KDCSVC_JSON" | jq -r ".pkinit_identity")
+export HCP_KDCSVC_KEY_ROTATION_EPOCH=$(echo "$HCP_KDCSVC_JSON" | jq -r ".key_rotation_epoch")
+export HCP_KDCSVC_KEY_ROTATION_PERIOD=$(echo "$HCP_KDCSVC_JSON" | jq -r ".key_rotation_period")
+export HCP_KDCSVC_MAX_TICKET_LIFE=$(echo "$HCP_KDCSVC_JSON" | jq -r ".max_ticket_life")
+export HCP_KDCSVC_MAX_RENEWABLE_LIFE=$(echo "$HCP_KDCSVC_JSON" | jq -r ".max_renewable_life")
 
 echo "Parsed 'kdcsvc': $HCP_HOSTNAME"
-echo "       STATE: $HCP_KDCSVC_STATE"
-echo " GLOBAL_INIT: $HCP_KDCSVC_GLOBAL_INIT"
-echo "  LOCAL_INIT: $HCP_KDCSVC_LOCAL_INIT"
-echo "        MODE: $HCP_KDCSVC_MODE"
-echo " SECONDARIES: $HCP_KDCSVC_SECONDARIES"
-echo "       REALM: $HCP_KDCSVC_REALM"
-echo "   NAMESPACE: $HCP_KDCSVC_NAMESPACE"
-echo "   POLICYURL: $HCP_KDCSVC_POLICYURL"
-echo "      ANCHOR: $HCP_KDCSVC_ANCHOR"
-echo "   PKINIT_ID: $HCP_KDCSVC_PKINIT_IDENTITY"
+echo "               STATE: $HCP_KDCSVC_STATE"
+echo "         GLOBAL_INIT: $HCP_KDCSVC_GLOBAL_INIT"
+echo "          LOCAL_INIT: $HCP_KDCSVC_LOCAL_INIT"
+echo "                MODE: $HCP_KDCSVC_MODE"
+echo "         SECONDARIES: $HCP_KDCSVC_SECONDARIES"
+echo "               REALM: $HCP_KDCSVC_REALM"
+echo "           NAMESPACE: $HCP_KDCSVC_NAMESPACE"
+echo "           POLICYURL: $HCP_KDCSVC_POLICYURL"
+echo "              ANCHOR: $HCP_KDCSVC_ANCHOR"
+echo "     PKINIT_IDENTITY: $HCP_KDCSVC_PKINIT_IDENTITY"
+echo "  KEY_ROTATION_EPOCH: $HCP_KDCSVC_KEY_ROTATION_EPOCH"
+echo " KEY_ROTATION_PERIOD: $HCP_KDCSVC_KEY_ROTATION_PERIOD"
+echo "     MAX_TICKET_LIFE: $HCP_KDCSVC_MAX_TICKET_LIFE"
+echo "  MAX_RENEWABLE_LIFE: $HCP_KDCSVC_MAX_RENEWABLE_LIFE"
 
 if [[ ! -d $HCP_KDCSVC_STATE ]]; then
 	echo "Error, kdcsvc::state isn't a directory: $HCP_KDCSVC_STATE" >&2
