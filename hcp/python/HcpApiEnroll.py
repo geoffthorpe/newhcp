0203dd6287b622b244cd4d89f6038a4cf0108702
Geoffrey Thorpe <geoff@geoffthorpe.net>
2025-08-03 20:17:44 -0400

    hcp: rework orchestrator into python
    This also modifies the HcpApiEnroll API to make it more usasble from other
    python code (which includes the new orchestrator). It also changes the
    fleet.json structure slightly.
    
    Signed-off-by: Geoffrey Thorpe <geoff@geoffthorpe.net>

--- hcp/python/HcpApiEnroll.py
+++ hcp/python/HcpApiEnroll.py
@@ -72,9 +72,8 @@ if os.environ.get('HCP_GSSAPI'):
 # pass along whatever it returned rather than retrying. (If the response
 # contains a non-2xx http status code, that's not our business.)
         #except requests.exceptions.RequestException as e:
-def requester_loop(args, request_fn):
-    debug(f"requester_loop: retries={args.retries}, pause={args.pause}")
-    retries = args.retries
+def requester_loop(request_fn, retries = 0, pause = 0):
+    debug(f"requester_loop: retries={retries}, pause={pause}")
     while True:
         try:
             debug("requester_loop: calling request_fn()")
@@ -84,7 +83,7 @@ def requester_loop(args, request_fn):
                 debug(f"requester_loop: caught exception, retries={retries}")
                 debug(f" - e: {e}")
                 retries -= 1
-                time.sleep(args.pause)
+                time.sleep(pause)
                 continue
             debug(f"requester_loop: caught exception, no retries")
             debug(f" - e: {e}")
@@ -96,24 +95,27 @@ def requester_loop(args, request_fn):
 # They all return a 2-tuple of {result,json}, where result is True iff the
 # operation was successful.
 
-def enroll_add(args):
+def enroll_add(api, ekpub, profile = None, requests_verify = True,
+               requests_cert = False, retries = 0, timeout = 120):
+    debug(f"enroll_add:")
+    debug(f"  api={api}")
+    debug(f"  ekpub={ekpub}")
+    debug(f"  profile={profile}")
+    debug(f"  requests_verify={requests_verify}")
+    debug(f"  requests_cert={requests_cert}")
     form_data = {
-        'ekpub': ('ek.pub', open(args.ekpub, 'rb'))
+        'ekpub': ('ek.pub', open(ekpub, 'rb'))
     }
-    if args.profile is not None:
-        form_data['profile'] = (None, args.profile)
-    debug("'add' handler about to call API")
-    debug(f" - url: {args.api + '/v1/add'}")
-    debug(f" - files: {form_data}")
-    myrequest = lambda: requests.post(args.api + '/v1/add',
-                             files=form_data,
-                             auth=auth,
-                             verify=args.requests_verify,
-                             cert=args.requests_cert,
-                             timeout=args.timeout)
-    response = requester_loop(args, myrequest)
+    if profile:
+        form_data['profile'] = (None, profile)
+    myrequest = lambda: requests.post(api + '/v1/add',
+                                      files = form_data,
+                                      auth = auth,
+                                      verify = requests_verify,
+                                      cert = requests_cert,
+                                      timeout = timeout)
+    response = requester_loop(myrequest, retries = retries)
     debug(f" - response: {response}")
-    debug(f" - response.content: {response.content}")
     if response.status_code != 201:
         err(f"Error, 'add' response status code was {response.status_code}")
         return False, None
@@ -125,18 +127,19 @@ def enroll_add(args):
     debug(f" - jr: {jr}")
     return True, jr
 
-def enroll_reenroll(args):
-    form_data = { 'ekpubhash': (None, args.ekpubhash) }
+def enroll_reenroll(api, ekpubhash, requests_verify = True,
+                    requests_cert = False, retries = 0, timeout = 120):
+    form_data = { 'ekpubhash': (None, ekpubhash) }
     debug("'reenroll' handler about to call API")
-    debug(f" - url: {args.api + '/v1/reenroll'}")
+    debug(f" - url: {api + '/v1/reenroll'}")
     debug(f" - files: {form_data}")
-    myrequest = lambda: requests.post(args.api + '/v1/reenroll',
-                             files=form_data,
-                             auth=auth,
-                             verify=args.requests_verify,
-                             cert=args.requests_cert,
-                             timeout=args.timeout)
-    response = requester_loop(args, myrequest)
+    myrequest = lambda: requests.post(api + '/v1/reenroll',
+                                      files = form_data,
+                                      auth = auth,
+                                      verify = requests_verify,
+                                      cert = requests_cert,
+                                      timeout = timeout)
+    response = requester_loop(myrequest, retries = retries)
     debug(f" - response: {response}")
     debug(f" - response.content: {response.content}")
     if response.status_code != 201:
@@ -150,34 +153,36 @@ def enroll_reenroll(args):
     debug(f" - jr: {jr}")
     return True, jr
 
-def do_query_or_delete(args, is_delete):
+def do_query_or_delete(api, ekpubhash, is_delete, nofiles,
+                       requests_verify = True, requests_cert = False,
+                       retries = 0, timeout = 120):
     form_data = {
-        'ekpubhash': (None, args.ekpubhash)
+        'ekpubhash': (None, ekpubhash)
     }
-    if args.nofiles:
+    if nofiles:
         form_data['nofiles'] = (None, True)
     if is_delete:
         debug("'delete' handler about to call API")
-        debug(f" - url: {args.api + '/v1/delete'}")
+        debug(f" - url: {api + '/v1/delete'}")
         debug(f" - files: {form_data}")
-        myrequest = lambda: requests.post(args.api + '/v1/delete',
-                                 files=form_data,
-                                 auth=auth,
-                                 verify=args.requests_verify,
-                                 cert=args.requests_cert,
-                                 timeout=args.timeout)
-        response = requester_loop(args, myrequest)
+        myrequest = lambda: requests.post(api + '/v1/delete',
+                                          files = form_data,
+                                          auth = auth,
+                                          verify = requests_verify,
+                                          cert = requests_cert,
+                                          timeout = timeout)
+        response = requester_loop(myrequest, retries = retries)
     else:
         debug("'query' handler about to call API")
-        debug(f" - url: {args.api + '/v1/query'}")
+        debug(f" - url: {api + '/v1/query'}")
         debug(f" - params: {form_data}")
-        myrequest = lambda: requests.get(args.api + '/v1/query',
-                                params=form_data,
-                                auth=auth,
-                                verify=args.requests_verify,
-                                cert=args.requests_cert,
-                                timeout=args.timeout)
-        response = requester_loop(args, myrequest)
+        myrequest = lambda: requests.get(api + '/v1/query',
+                                         params = form_data,
+                                         auth = auth,
+                                         verify = requests_verify,
+                                         cert = requests_cert,
+                                         timeout = timeout)
+        response = requester_loop(myrequest, retries = retries)
     debug(f" - response: {response}")
     debug(f" - response.content: {response.content}")
     if response.status_code != 200:
@@ -191,21 +196,22 @@ def do_query_or_delete(args, is_delete):
     debug(f" - jr: {jr}")
     return True, jr
 
-def enroll_query(args):
-    return do_query_or_delete(args, False)
+def enroll_query(api, ekpubhash, nofiles, **kwargs):
+    return do_query_or_delete(api, ekpubhash, False, nofiles, **kwargs)
 
-def enroll_delete(args):
-    return do_query_or_delete(args, True)
+def enroll_delete(api, ekpubhash, nofiles, **kwargs):
+    return do_query_or_delete(api, ekpubhash, True, nofiles, **kwargs)
 
-def enroll_janitor(args):
+def enroll_janitor(api, requests_verify = True, requests_cert = False,
+                   retries = 0, timeout = 120):
     debug("'janitor' handler about to call API")
-    debug(f" - url: {args.api + '/v1/janitor'}")
-    myrequest = lambda: requests.get(args.api + '/v1/janitor',
-                            auth=auth,
-                            verify=args.requests_verify,
-                            cert=args.requests_cert,
-                            timeout=args.timeout)
-    response = requester_loop(args, myrequest)
+    debug(f" - url: {api + '/v1/janitor'}")
+    myrequest = lambda: requests.get(api + '/v1/janitor',
+                                     auth = auth,
+                                     verify = requests_verify,
+                                     cert = requests_cert,
+                                     timeout = timeout)
+    response = requester_loop(myrequest, retries = retries)
     debug(f" - response: {response}")
     debug(f" - response.content: {response.content}")
     if response.status_code != 200:
@@ -287,7 +293,7 @@ if __name__ == '__main__':
     parser_a = subparsers.add_parser('add', help=add_help, epilog=add_epilog)
     parser_a.add_argument('ekpub', help=add_help_ekpub)
     parser_a.add_argument('--profile', help=add_help_profile, required=False)
-    parser_a.set_defaults(func=enroll_add)
+    parser_a.set_defaults(func='add')
 
     reenroll_help = 'Re-enroll a TPM/host based on hash(EKpub)'
     reenroll_epilog = """
@@ -297,7 +303,7 @@ if __name__ == '__main__':
     reenroll_help_ekpubhash = 'hexidecimal "ekpubhash" of the TPM'
     parser_d = subparsers.add_parser('reenroll', help=reenroll_help, epilog=reenroll_epilog)
     parser_d.add_argument('ekpubhash', help=reenroll_help_ekpubhash)
-    parser_d.set_defaults(func=enroll_reenroll)
+    parser_d.set_defaults(func='reenroll')
 
     query_help = 'Query (and list) enrollments based on prefix-search of hash(EKpub)'
     query_epilog = """
@@ -318,7 +324,7 @@ if __name__ == '__main__':
     parser_q = subparsers.add_parser('query', help=query_help, epilog=query_epilog)
     parser_q.add_argument('ekpubhash', help=query_help_ekpubhash)
     parser_q.add_argument('--nofiles', action='store_true', help=query_help_nofiles)
-    parser_q.set_defaults(func=enroll_query)
+    parser_q.set_defaults(func='query')
 
     delete_help = 'Delete enrollments based on prefix-search of hash(EKpub)'
     delete_epilog = """
@@ -334,7 +340,7 @@ if __name__ == '__main__':
     parser_d = subparsers.add_parser('delete', help=delete_help, epilog=delete_epilog)
     parser_d.add_argument('ekpubhash', help=delete_help_ekpubhash)
     parser_d.add_argument('--nofiles', action='store_true', help=query_help_nofiles)
-    parser_d.set_defaults(func=enroll_delete)
+    parser_d.set_defaults(func='delete')
 
     janitor_help = 'Scrub the enrollment DB to fix known issues, and rebuild hn2ek'
     janitor_epilog = """
@@ -345,7 +351,7 @@ if __name__ == '__main__':
     from first principles.
     """
     parser_j = subparsers.add_parser('janitor', help=janitor_help, epilog=janitor_epilog)
-    parser_j.set_defaults(func=enroll_janitor)
+    parser_j.set_defaults(func='janitor')
 
     # Process the command-line
     args = parser.parse_args()
@@ -354,26 +360,50 @@ if __name__ == '__main__':
     if not args.api:
         err("Error, no API URL was provided.")
         sys.exit(-1)
+    requests_verify = True
     if args.noverify:
-        args.requests_verify = False
+        requests_verify = False
     elif args.cacert:
-        args.requests_verify = args.cacert
-    else:
-        args.requests_verify = True
+        requests_verify = args.cacert
+    requests_cert = False
     if args.clientcert:
         if args.clientkey:
-            args.requests_cert = (args.clientcert,args.clientkey)
+            requests_cert = (args.clientcert,args.clientkey)
         else:
-            args.requests_cert = args.clientcert
-    else:
-        args.requests_cert = False
+            requests_cert = args.clientcert
 
     # Dispatch. Here, we are operating as a program with a parent process, not a library with
     # a caller. If we don't catch exceptions, they get handled by the runtime which will print
     # stack traces to stderr and exit, there is no concept of the exceptions reaching the
     # parent process. As such, catch them all here.
     try:
-        result, j = args.func(args)
+        if args.func == 'add':
+            result, j = enroll_add(args.api, args.ekpub, args.profile,
+                                   requests_verify = requests_verify,
+                                   requests_cert = requests_cert,
+                                   timeout = args.timeout)
+        elif args.func == 'reenroll':
+            result, j = enroll_reenroll(args.api, args.ekpubhash,
+                                   requests_verify = requests_verify,
+                                   requests_cert = requests_cert,
+                                   timeout = args.timeout)
+        elif args.func == 'query':
+            result, j = enroll_query(args.api, args.ekpubhash, args.nofiles,
+                                   requests_verify = requests_verify,
+                                   requests_cert = requests_cert,
+                                   timeout = args.timeout)
+        elif args.func == 'delete':
+            result, j = enroll_delete(args.api, args.ekpubhash, args.nofiles,
+                                   requests_verify = requests_verify,
+                                   requests_cert = requests_cert,
+                                   timeout = args.timeout)
+        elif args.func == 'janitor':
+            result, j = enroll_janitor(args.api,
+                                   requests_verify = requests_verify,
+                                   requests_cert = requests_cert,
+                                   timeout = args.timeout)
+        else:
+            raise Exception("BUG")
     except Exception as e:
         print(f"Error, unable to hit API end-point: {e}", file = sys.stderr)
         sys.exit(-1)
---
+++ hcp/python/HcpToolOrchestrator.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python3
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+import os
+import sys
+import json
+import subprocess
+import time
+import tempfile
+import shutil
+import argparse
+
+from HcpCommon import bail, log, hlog, \
+    hcp_config_extract, hcp_config_scope_get, hcp_config_scope_set, \
+    hcp_config_scope_shrink
+from HcpRecursiveUnion import union
+import HcpApiEnroll
+
+fleetconfpath = hcp_config_extract('.orchestrator.fleet', must_exist = True)
+if not os.path.isfile(fleetconfpath):
+    bail(f"No config at '{fleetconfpath}'")
+with open(fleetconfpath, 'r') as fp:
+    fleetconf = json.loads(fp.read())
+fleetdefaults = fleetconf.pop('defaults') if 'defaults' in fleetconf else {}
+fleet = fleetconf.pop('fleet') if 'fleet' in fleetconf else {}
+fleethosts = [ name for name in fleet if name != '_']
+
+class FleetHost:
+    def post_exist(self):
+        c = subprocess.run(['openssl', 'sha256', '-r', self.ekpub],
+                           capture_output = True, text = True)
+        if c.returncode != 0:
+            raise Exception("Failed to hash ekpub")
+        self.ekpubhash = c.stdout[0:64]
+        retcode, result = HcpApiEnroll.enroll_query(self.api,
+                                                    self.ekpubhash,
+                                                    True,
+                                                    requests_verify = self.api_cacert,
+                                                    requests_cert = self.api_clientcert)
+        if not retcode or 'entries' not in result:
+            raise Exception("Failed to query enrollsvc")
+        self.enrolled = len(result['entries']) > 0
+    def __init__(self, name):
+        if name == 'defaults':
+            raise Exception("'defaults' is an illegal fleet host name")
+        if name not in fleet:
+            raise Exception(f"Unknown fleet host '{name}'")
+        self.name = name
+        self.profile = union(fleetdefaults, fleet[name])
+        self.api = self.profile['enroll_api']
+        self.api_cacert = self.profile['enroll_api_cacert']
+        self.api_clientcert = self.profile['enroll_api_clientcert'] \
+            if 'enroll_api_clientcert' in self.profile else False
+        self.tpm_create = self.profile['tpm_create']
+        self.tpm_enroll = self.profile['tpm_enroll']
+        self.tpm_path = self.profile['tpm_path']
+        self.enroll_profile = self.profile['enroll_profile']
+        self.ekpub = f"{self.tpm_path}/tpm/ek.pub"
+        self.exists = os.path.isfile(self.ekpub)
+        self.enrolled = False
+        if self.exists:
+            self.post_exist()
+    def create(self):
+        if self.exists:
+            print(f"{self.name}: TPM already exists")
+            return
+        os.makedirs(f"{self.tpm_path}/tpm-temp", exist_ok = True)
+        c = subprocess.run(['swtpm_setup', '--tpm2',
+                            '--createek',
+                            '--tpmstate', f"{self.tpm_path}/tpm-temp",
+                            '--config', '/dev/null'],
+                           capture_output = True)
+        if c.returncode != 0:
+            print(f"Error, swtpm_setup failed for '{self.name}'")
+            return
+        with tempfile.TemporaryDirectory() as sockdir:
+            p = subprocess.Popen(['swtpm', 'socket', '--tpm2',
+                                  '--tpmstate', f"dir={self.tpm_path}/tpm-temp",
+                                  '--server', f"type=unixio,path={sockdir}/tpm",
+                                  '--ctrl', f"type=unixio,path={sockdir}/tpm.ctrl",
+                                  '--flags', 'startup-clear'],
+                                 stdout = subprocess.PIPE,
+                                 stderr = subprocess.PIPE)
+            if p.poll() != None:
+                print(f"Error, swtpm temp-start failed for '{self.name}'")
+                return
+            newenv = os.environ.copy()
+            newenv['TPM2TOOLS_TCTI'] = f"swtpm:path={sockdir}/tpm"
+            c = subprocess.run(['tpm2', 'createek',
+                                f"--ek-context={self.tpm_path}/tpm-temp/ek.ctx",
+                                f"--public={self.tpm_path}/tpm-temp/ek.pub",
+                                '--key-algorithm=rsa'],
+                               env = newenv,
+                               capture_output = True)
+            p.kill()
+            if c.returncode != 0:
+                print(f"Error, swtpm createek failed for '{self.name}'")
+                return
+        c = subprocess.run(['tpm2', 'print',
+                            '-t', 'TPM2B_PUBLIC',
+                            '-f', 'PEM',
+                            f"{self.tpm_path}/tpm-temp/ek.pub"],
+                           capture_output = True)
+        if c.returncode != 0:
+            print(f"Error, conversion TPM2B_PUBLIC to PEM failed for '{self.name}'")
+            return
+        shutil.move(f"{self.tpm_path}/tpm-temp",
+                    f"{self.tpm_path}/tpm")
+        self.exists = True
+        self.post_exist()
+        print(f"{self.name}: TPM created")
+    def delete(self):
+        if not self.exists:
+            print(f"{self.name}: TPM doesn't exist")
+            return
+        shutil.rmtree(f"{self.tpm_path}/tpm")
+        self.exists = False
+        print(f"{self.name}: TPM deleted")
+    def enroll(self):
+        if not self.exists:
+            print(f"{self.name}: TPM doesn't exist")
+            return
+        if self.enrolled:
+            print(f"{self.name}: TPM already enrolled")
+            return
+        retcode, result = HcpApiEnroll.enroll_add(self.api,
+                                                  self.ekpub,
+                                                  profile = json.dumps(self.enroll_profile),
+                                                  requests_verify  = self.api_cacert,
+                                                  requests_cert = self.api_clientcert)
+        if not retcode:
+            raise Exception("Failed to get enrollsvc response")
+        if 'ekpubhash' not in result:
+            print(f"Error, TPM enrollment not processed for '{self.name}'")
+            return
+        print(f"{self.name}: TPM enrolled")
+        self.enrolled = True
+    def unenroll(self):
+        if not self.exists:
+            print(f"{self.name}: TPM doesn't exist")
+            return
+        if not self.enrolled:
+            print(f"{self.name}: TPM isn't enrolled")
+        retcode, result = HcpApiEnroll.enroll_delete(self.api,
+                                                     self.ekpubhash,
+                                                     True,
+                                                     requests_verify = self.api_cacert,
+                                                     requests_cert = self.api_clientcert)
+        if not retcode or 'entries' not in result:
+            raise Exception("Failed to get enrollsvc response")
+        if len(result['entries']) == 0:
+            print(f"Error, TPM unenrollment not processed for '{self.name}'")
+            return
+        print(f"{self.name}: TPM unenrolled")
+        self.enrolled = False
+    def status(self):
+        if self.exists:
+            if self.enrolled:
+                status = 'exists, enrolled'
+            else:
+                status = 'exists, not enrolled'
+        else:
+            status = 'non-existant'
+        print(f"{self.name}: TPM {status}")
+
+if __name__ == '__main__':
+
+    # Wrapper command, using argparse
+    _desc = 'Orchestrator tool for Enroll Service and SWTPM management'
+    _epilog = """
+    Using the fleet.json config file, this tool can manage swtpm instances
+    and perform enroll/unenroll actions against the Enrollment Service
+    management interface.
+    """
+    _help_create = 'The host\'s TPM will be created if it doesn\'t exist'
+    _help_delete = 'The host\'s TPM will be deleted if it exists'
+    _help_enroll = 'The host will be enrolled if it\'s not already'
+    _help_unenroll = 'The host will be unenrolled if it\'s enrolled'
+    _help_verbosity = 'Verbosity level, 0 means quiet, more than 0 means less quiet'
+    _help_hostnames = 'Names matching stanzas found in fleet.json'
+    parser = argparse.ArgumentParser(description = _desc,
+                                     epilog = _epilog)
+    parser.add_argument('-c', '--create', action='store_true', help = _help_create)
+    parser.add_argument('-d', '--delete', action='store_true', help = _help_delete)
+    parser.add_argument('-e', '--enroll', action='store_true', help = _help_enroll)
+    parser.add_argument('-u', '--unenroll', action='store_true', help = _help_unenroll)
+    parser.add_argument('-v', '--verbosity', help = _help_verbosity)
+    parser.add_argument('hostnames', nargs='*', help = _help_hostnames)
+
+    args = parser.parse_args()
+    if args.verbosity:
+        HcpApiEnroll.set_loglevel(int(args.verbosity))
+    if args.create and args.delete:
+        raise Exception("Cannot create and delete in the same command")
+    if args.enroll and args.unenroll:
+        raise Exception("Cannot enroll and unenroll in the same command")
+    if len(args.hostnames) == 0:
+        args.hostnames = fleethosts
+    for host in args.hostnames:
+        fh = FleetHost(host)
+        if args.create:
+            fh.create()
+        if args.delete:
+            fh.delete()
+        if args.enroll:
+            fh.enroll()
+        if args.unenroll:
+            fh.unenroll()
+        if not args.create and not args.delete and not args.enroll and not args.unenroll:
+            fh.status()
