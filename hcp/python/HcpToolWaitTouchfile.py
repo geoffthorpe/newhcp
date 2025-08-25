8fdcd190efbf3f74dcce27e46c64c03baab1c543
Geoffrey Thorpe <geoff@geoffthorpe.net>
2025-08-24 21:24:09 -0400

    python/HcpToolWaitTouchfile: replace implementation
    Signed-off-by: Geoffrey Thorpe <geoff@geoffthorpe.net>

--- hcp/python/HcpToolWaitTouchfile.py
+++ hcp/python/HcpToolWaitTouchfile.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python3
-# vim: set expandtab shiftwidth=4 softtabstop=4:
 
 import os
 import sys
+import subprocess
+import argparse
 import time
+import shutil
 
-from HcpCommon import bail, log, hlog
+import HcpCommon as h
 
-if len(sys.argv) < 2:
-	bail("'HcpToolWaitTouchfile' expects a path argument")
+try:
+	verbosity = int(os.environ['VERBOSE'])
+except:
+	verbosity = 1
 
-p = sys.argv[1]
-log(f"HcpToolWaitTouchfile({p}) starting")
+parser = argparse.ArgumentParser()
+parser.add_argument("-R", "--retries", type = int, default = -1,
+		help = "for healthcheck, max # of retries")
+parser.add_argument("-P", "--pause", type = int, default = 1,
+		help = "for healthcheck, pause (seconds) between retries")
+parser.add_argument("-v", "--verbose", default = 0, action = "count",
+		help = "increase output verbosity")
+parser.add_argument("-V", "--less-verbose", default = 0, action = "count",
+		help = "decrease output verbosity")
+parser.add_argument('path', help = "path to poll for")
+args = parser.parse_args()
+verbosity = verbosity + args.verbose - args.less_verbose
+h.current_loglevel = verbosity
+os.environ['VERBOSE'] = f"{verbosity}"
 
-while not os.path.isfile(p):
-	hlog(2, f"touchfile {p} doesn't exist yet")
-	time.sleep(1)
-
-log(f"HcpToolWaitTouchfile({p}) done")
+h.hlog(1, f"Running attested.py")
+while not os.path.isfile(args.path):
+	h.hlog(1, f"Doesn't exist yet")
+	if args.retries == 0:
+		h.hlog(0, "Failure, giving up")
+		sys.exit(1)
+	args.retries = args.retries - 1
+	if args.pause > 0:
+		h.hlog(2, f"Pausing for {args.pause} seconds")
+		time.sleep(args.pause)
