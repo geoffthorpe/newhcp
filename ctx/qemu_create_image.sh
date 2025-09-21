6abc7c2c751d636b66af5716ed075fbaafb68b10
Geoffrey Thorpe <geoff@geoffthorpe.net>
2025-09-20 20:30:37 -0400

    hcp: reduce the bling of VM image creation
    Signed-off-by: Geoffrey Thorpe <geoff@geoffthorpe.net>

--- ctx/qemu_create_image.sh
+++ ctx/qemu_create_image.sh
@@ -10,20 +10,12 @@ VM_DISK_SIZE_MB=$3
 
 IMG=/crud/hcp_qemu_guest.img
 
-echo_blue() {
-    local font_blue="\033[94m"
-    local font_bold="\033[1m"
-    local font_end="\033[0m"
-
-    echo -e "\n${font_blue}${font_bold}${1}${font_end}"
-}
-
-echo_blue "[Create disk image]"
+echo "[Create disk image]"
 [ -z "${VM_DISK_SIZE_MB}" ] && VM_DISK_SIZE_MB=1024
 VM_DISK_SIZE_SECTOR=$(expr $VM_DISK_SIZE_MB \* 1024 \* 1024 / 512)
 dd if=/dev/zero of=${IMG} bs=${VM_DISK_SIZE_SECTOR} count=512
 
-echo_blue "[Make partition]"
+echo "[Make partition]"
 sfdisk ${IMG} <<EOF
 label: dos
 label-id: 0xdeadbeef
@@ -32,33 +24,28 @@ unit: sectors
 linux-part : start=2048, type=83, bootable
 EOF
 
-echo_blue "\n[Format partition with ext4]"
+echo "[Format partition with ext4]"
 losetup -D
 LOOPDEVICE=$(losetup -f)
 echo -e "\n[Using ${LOOPDEVICE} loop device]"
 losetup -o $(expr 512 \* 2048) ${LOOPDEVICE} ${IMG}
 mkfs.ext4 ${LOOPDEVICE}
 
-echo_blue "[Copy directory structure to partition]"
+echo "[Copy directory structure to partition]"
 mkdir -p /tmpmnt
 mount -t auto ${LOOPDEVICE} /tmpmnt/
 cp -a /poo/. /tmpmnt/
 
-echo_blue "[Setup extlinux]"
+echo "[Setup extlinux]"
 extlinux --install /tmpmnt/boot/
 cp /syslinux.cfg /tmpmnt/boot/syslinux.cfg
 rm /tmpmnt/.dockerenv
 
-echo_blue "[Unmount]"
+echo "[Unmount]"
 umount /tmpmnt
 losetup -D
 
-echo_blue "[Write syslinux MBR]"
+echo "[Write syslinux MBR]"
 dd if=/usr/lib/syslinux/mbr/mbr.bin of=${IMG} bs=440 count=1 conv=notrunc
 
-#echo_blue "[Convert to qcow2]"
-#qemu-img convert -c ${IMG} -O qcow2 ${IMG}.qcow2
-
-[ "${UID_HOST}" -a "${GID_HOST}" ] && chown ${UID_HOST}:${GID_HOST} ${IMG} #${IMG}.qcow2
-
-#rm -r /os/${DISTR}.dir
+[ "${UID_HOST}" -a "${GID_HOST}" ] && chown ${UID_HOST}:${GID_HOST} ${IMG}
--- ctx/uml_create_image.sh
+++ ctx/uml_create_image.sh
@@ -10,39 +10,26 @@ VM_DISK_SIZE_MB=$3
 
 IMG=/crud/hcp_uml_guest.img
 
-echo_blue() {
-    local font_blue="\033[94m"
-    local font_bold="\033[1m"
-    local font_end="\033[0m"
-
-    echo -e "\n${font_blue}${font_bold}${1}${font_end}"
-}
-
-echo_blue "[Create disk image]"
+echo "[Create disk image]"
 [ -z "${VM_DISK_SIZE_MB}" ] && VM_DISK_SIZE_MB=1024
 VM_DISK_SIZE_SECTOR=$(expr $VM_DISK_SIZE_MB \* 1024 \* 1024 / 512)
 dd if=/dev/zero of=${IMG} bs=${VM_DISK_SIZE_SECTOR} count=512
 
-echo_blue "\n[Format image with ext4]"
+echo "[Format image with ext4]"
 mkfs.ext4 ${IMG}
 
-echo_blue "\n[Mounting ext4 partition by loopback]"
+echo "[Mounting ext4 partition by loopback]"
 losetup -D
 LOOPDEVICE=$(losetup -f)
-echo -e "\n[Using ${LOOPDEVICE} loop device]"
+echo -e "[Using ${LOOPDEVICE} loop device]"
 losetup ${LOOPDEVICE} ${IMG}
 mkdir -p /tmpmnt
 mount -t auto ${LOOPDEVICE} /tmpmnt/
 
-echo_blue "[Copy directory structure to partition]"
+echo "[Copy directory structure to partition]"
 cp -a /poo/. /tmpmnt/
 
-#echo_blue "[Setup extlinux]"
-#extlinux --install /tmpmnt/boot/
-#cp /syslinux.cfg /tmpmnt/boot/syslinux.cfg
-#rm /tmpmnt/.dockerenv
-
-echo_blue "[Unmount]"
+echo "[Unmount]"
 umount /tmpmnt
 losetup -D
 
