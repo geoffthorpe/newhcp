2e7dd1b63c703d48ff5df8be554ae134aeee27e2
Geoffrey Thorpe <geoff@geoffthorpe.net>
2025-09-20 20:30:37 -0400

    usecase: fork the current "main" json and test for "x"
    Signed-off-by: Geoffrey Thorpe <geoff@geoffthorpe.net>

---
+++ usecase/x-test.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+[[ -z $TOP ]] && echo "Fail: must export TOP=\$(pwd)!" && exit 1 || true
+
+[[ -z $PROJECT ]] && export PROJECT=$(basename $(pwd)) && \
+	echo "WARN: no PROJECT set, defaulting to '$PROJECT'"
+DCFLAGS="-p $PROJECT"
+
+DOMAIN=$(jq -r .vars.domain usecase/fleet.json)
+
+echo "Running bringup"
+
+[[ -n $V ]] && OUT=/dev/stdout || OUT=/dev/null
+[[ -n $Q ]] && ERR=/dev/null || ERR=/dev/stderr
+
+do_run() {
+	FLAGS=""
+	_command=$1
+	shift
+	BACKQ=$Q
+	BACKOUT=$OUT
+	if [[ $_command == "up" ]]; then
+		FLAGS="$FLAGS -d"
+	elif [[ $_command == "run" ]]; then
+		FLAGS="$FLAGS --rm"
+	elif [[ $_command == "down" ]]; then
+		FLAGS="$FLAGS -v"
+	elif [[ $_command == "exec" ]]; then
+		true
+	elif [[ $_command == "execT" ]]; then
+		_command="exec"
+		FLAGS="$FLAGS -T"
+		Q=yes
+		OUT=/dev/stdout
+	else
+		echo "Error: unknown cmd: $_command" >&2
+		exit 1
+	fi
+
+	[[ -n $Q ]] || echo "--> docker-compose $DCFLAGS $_command $FLAGS $@"
+	docker-compose $DCFLAGS $_command $FLAGS $@ > $OUT 2> $ERR && \
+		([[ -n $Q ]] || echo "--> SUCCESS") || \
+		exit 1
+	Q=$BACKQ
+	OUT=$BACKOUT
+}
+
+do_exit() {
+	(Q=1 do_run down "")
+}
+
+[[ -n $NOTRAP ]] || trap do_exit EXIT
+
+echo "Destroying any existing state"
+do_run down
+
+echo "Success"
