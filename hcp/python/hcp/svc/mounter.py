380ce538f32b0759301446f2de3045fe431c6073
Geoffrey Thorpe <geoff@geoffthorpe.net>
2025-09-20 20:30:37 -0400

    hcp: add 'mounter' service and make 'catarina' use it
    The counterpart of 'nfsd', this allows a host to mount NFS volumes.
    'catarina' mounts the /home directory from 'nfs'.
    
    Signed-off-by: Geoffrey Thorpe <geoff@geoffthorpe.net>

---
+++ hcp/python/hcp/svc/mounter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# vim: set expandtab shiftwidth=4 softtabstop=4: 
+
+import sys
+import os
+import subprocess
+import time
+
+from hcp.common import hcp_config_extract
+
+_mounts = hcp_config_extract('.mounter.mounts', or_default = True)
+
+print(f"Starting mounter")
+print(f" - mounts={_mounts}")
+
+if _mounts:
+    print('Updating /etc/fstab')
+    with open('/etc/fstab', 'a') as fp:
+        for tgt in _mounts:
+            src = _mounts[tgt]
+            print(f"  {tgt}  <--  {src}")
+            fp.write(f"{src} {tgt} nfs defaults 0 0\n")
+    print('Running \'systemctl daemon-reload\'')
+    subprocess.run(['systemctl', 'daemon-reload'])
+    print('Running \'mount -a\'')
+    subprocess.run(['mount', '-a'])
+else:
+    print('Nothing to mount')
