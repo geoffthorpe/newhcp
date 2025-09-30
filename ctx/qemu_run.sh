5359d8b302e3371fa9670ce7c4c97cc4f3fb5c9c
Geoffrey Thorpe <geoff@geoffthorpe.net>
2025-09-29 23:42:18 -0400

    hcp: bring qemu support up to date with uml
    Signed-off-by: Geoffrey Thorpe <geoff@geoffthorpe.net>

--- ctx/qemu_run.sh
+++ ctx/qemu_run.sh
@@ -8,11 +8,23 @@ dd if=/dev/zero of=/swapfile bs=1M count=1024
 chmod 600 /swapfile
 mkswap /swapfile
 
+# HACK: NFSv4 over NAT is complicated by the concept of server callbacks. The
+# client VM needs to know its container's IP address when mounting.
+hostip=$(ip addr show scope global | grep inet | \
+	sed -e "s/^.*inet //" | sed -e "s,/.*,,")
+echo "Host IP: $hostip"
+
+# HACK: to reduce the chance of VMs having conflicting source IP addresses,
+# randomize their choice of subnet
+randomid=$(($RANDOM % 200 + 2))
+echo "Using random subnet 10.0.$randomid.2/24"
+
 mkdir -p /_env
 
 echo "export PYTHONPATH=$PYTHONPATH" > /_env/env
 echo "export HCP_CONFIG_MUTATE=$VM_HCP_CONFIG_MUTATE" >> /_env/env
 echo "export HOSTNAME=$(hostname)" >> /_env/env
+echo "export HOSTIP=$hostip" >> /_env/env
 echo "Set up VM 'env' file" >&2
 
 vde_switch --sock=/tmp/myswitch --daemon
@@ -22,7 +34,7 @@ slirpvde -sock=/tmp/myswitch -dhcp -daemon \
 	-L 22:10.0.2.15:22 \
 	-L 443:10.0.2.15:443 \
 	-L 2049:10.0.2.15:2049 \
-	-L 111:10.0.2.15:111
+	-L 32764:10.0.$randomid.15:32764
 # vde_plug -d vde:///tmp/myswitch slirp:///tcpfwd=22:10.0.2.15:22/tcpfwd=443:10.0.2.15:443
 echo "Started VDE2 DHCP+DNS+NAT" >&2
 
