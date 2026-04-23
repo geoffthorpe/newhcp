b3b022a05b9898f1bb60967585bbd54574732a39
Geoffrey Thorpe <geoff@geoffthorpe.net>
2026-04-22 21:30:35 -0400

    mit: extend use of MIT to VMs
    Signed-off-by: Geoffrey Thorpe <geoff@geoffthorpe.net>

--- hcp/bash/shell.sh
+++ hcp/bash/shell.sh
@@ -4,7 +4,11 @@ if [[ -z $KRB5CCNAME ]]; then
 	ME=$(whoami)
 	if [[ -f /assets/pkinit-client-$ME.pem ]]; then
 		echo "Auto-running kinit to get TGT for '$ME'" >&2
-		exec /install-heimdal/bin/kinit -C FILE:/assets/pkinit-client-$ME.pem $ME bash "$@"
+		if [[ -d /install-kstart ]]; then
+			exec /install-kstart/bin/k5start -q -X X509_user_identity=FILE:/assets/pkinit-client-$ME.pem $ME -- bash "$@"
+		else
+			exec /install-heimdal/bin/kinit -C FILE:/assets/pkinit-client-$ME.pem $ME bash "$@"
+		fi
 	else
 		echo "No kinit/TGT available for '$ME'" >&2
 	fi
